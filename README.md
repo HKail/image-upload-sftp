fa82aa14996d55b890fcda66204dd0fc2e4e8f44
Allen <934932687@qq.com>
2019-05-28 23:04:43 +0800

    First commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+# SFTP image upload project.
+# SFTP图片上传项目.
+## [2019-05-28 23:00:59] 创建项目.
---
+++ src/main/java/com/miracle/upload/ImageUploadSftpApplication.java
@@ -0,0 +1,13 @@
+package com.miracle.upload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageUploadSftpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImageUploadSftpApplication.class, args);
+    }
+
+}
